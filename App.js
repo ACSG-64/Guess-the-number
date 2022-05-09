39b1fa41b54a9d9371e25e53a2d4cef67da97675
Andrés Sarmiento <acamilo707@gmail.com>
2022-05-09 00:17:48 -0500

    New fonts, global states and logic to navigate between screens were added

--- App.js
+++ App.js
@@ -1,8 +1,58 @@
+import {useState} from "react";
 import {ImageBackground, StyleSheet} from 'react-native';
-import StartGame from "./screens/StartGame";
+import {SafeAreaView} from "react-native-safe-area-context";
 import {LinearGradient} from "expo-linear-gradient";
+import {useFonts} from "expo-font";
+
+import StartGame from "./screens/StartGame";
+import InGame from "./screens/InGame";
+import GameOver from "./screens/GameOver";
+import AppLoading from "expo-app-loading";
 
 export default function App() {
+    const [currentScreen, setCurrentScreen] = useState('StartGame');
+    const [selectedNumber, setSelectedNumber] = useState(-1);
+    const [attemptsNumber, setAttemptsNumber] = useState(-1);
+
+    const [fontsLoaded] = useFonts({
+        "open-sans": require('./assets/fonts/OpenSans-Regular.ttf'),
+        "open-sans-bold": require('./assets/fonts/OpenSans-Bold.ttf'),
+    });
+
+    if (!fontsLoaded) return <AppLoading/>;
+
+    const navigateToStartGameScreen = () => {
+        setSelectedNumber(-1);
+        setAttemptsNumber(0);
+        setCurrentScreen('StartGame');
+    }
+
+    const navigateToInGameScreen = (inputNumber) => {
+        setSelectedNumber(inputNumber);
+        setCurrentScreen('InGame');
+    }
+
+    const navigateToGameOverScreen = (attemptsNumber) => {
+        setAttemptsNumber(attemptsNumber);
+        setCurrentScreen('GameOver');
+    }
+
+    let currentScreenComponent;
+    switch (currentScreen) {
+        case 'InGame':
+            currentScreenComponent =
+                <InGame selectedNumber={selectedNumber} navigateToGameOver={navigateToGameOverScreen}/>;
+            break;
+        case 'GameOver':
+            currentScreenComponent =
+                <GameOver guessedNumber={selectedNumber} attemptsNumber={attemptsNumber}
+                          navigateToStartGame={navigateToStartGameScreen}/>;
+            break;
+        default:
+            currentScreenComponent = <StartGame navigateToStartGame={navigateToInGameScreen}/>;
+            break;
+    }
+
     return (
         <LinearGradient colors={['#4e0329', '#ddb52f']} style={styles.screen}>
             <ImageBackground
@@ -10,7 +60,9 @@ export default function App() {
                 source={require('./assets/images/background.png')}
                 resizeMode="cover"
                 imageStyle={styles.backgroundImage}>
-                <StartGame/>
+                <SafeAreaView style={styles.screen}>
+                    {currentScreenComponent}
+                </SafeAreaView>
             </ImageBackground>
         </LinearGradient>
     );
