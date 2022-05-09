1f23b702e83ff0989a22b1cacbb704ac7317c7c8
Andrés Sarmiento <acamilo707@gmail.com>
2022-05-09 00:16:07 -0500

    Interface and logic were added to InGame and GameOver screens

--- screens/GameOver.js
+++ screens/GameOver.js
@@ -1,3 +1,57 @@
-function GameOver() {
+import {Image, StyleSheet, Text, View} from "react-native";
 
+import Title from "../components/ui/Title";
+import PrimaryButton from "../components/ui/PrimaryButton";
+import Colors from "../values/colors";
+
+export default function GameOver({attemptsNumber, guessedNumber, navigateToStartGame}) {
+    return (
+        <View style={styles.screen}>
+            <Title>Game over!</Title>
+            <View style={styles.imageContainer}>
+                <Image style={styles.image} source={require('../assets/images/robot-aha.jpg')}/>
+            </View>
+            <Text style={styles.feedbackText}>
+                The computer took <Text style={styles.highlightedText}>{attemptsNumber}</Text> attempts to guess
+                the number <Text style={styles.highlightedText}>{guessedNumber}</Text>
+            </Text>
+            <PrimaryButton onPress={navigateToStartGame}>
+                Go to the main screen
+            </PrimaryButton>
+        </View>
+    );
 }
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: '10%',
+    },
+    imageContainer: {
+        height: 215,
+        width: 215,
+        borderRadius: 200,
+        overflow: 'hidden',
+        margin: 10,
+        alignItems: 'center',
+    },
+    image: {
+        height: '100%',
+        width: '100%',
+    },
+    feedbackText: {
+        fontSize: 17,
+        fontFamily: 'open-sans',
+        color: 'white',
+        textAlign: 'center',
+        marginTop: 18,
+        marginBottom: 26,
+    },
+    highlightedText: {
+        color: Colors.deepCarmine,
+        fontFamily: 'open-sans-bold',
+        fontWeight: 'bold'
+    }
+});
--- screens/InGame.js
+++ screens/InGame.js
@@ -1,3 +1,109 @@
-function InGame() {
+import {FlatList, Text, View, StyleSheet, Alert} from "react-native";
+import Title from "../components/ui/Title";
+import {useEffect, useState} from "react";
+import NumberContainer from "../components/game/NumberContainer";
+import PrimaryButton from "../components/ui/PrimaryButton";
+import GuessAttemptLog from "../components/game/GuessAttemptLog";
 
+const guessBounds = {
+    min: 1,
+    max: 99,
+    reset: function () {
+        this.min = 1;
+        this.max = 99;
+    }
 }
+
+export default function InGame({selectedNumber, navigateToGameOver}) {
+    const initialGuess = generateRandomNumber(guessBounds.min, guessBounds.max, selectedNumber);
+    const [currentGuess, setCurrentGuess] = useState(initialGuess);
+    const [guessAttempts, setGuessAttempts] = useState([initialGuess]);
+    const [totalGuessAttempts, setTotalGuessAttempts] = useState(1);
+
+    useEffect(() => {
+        if (currentGuess === selectedNumber)
+            navigateToGameOver(totalGuessAttempts);
+    }, [currentGuess, totalGuessAttempts])
+
+    useEffect(() => guessBounds.reset(), [])
+
+    const nextGuessHandler = (bound) => {
+        if (bound === 'lower' && selectedNumber < currentGuess) {
+            guessBounds.max = currentGuess;
+        } else if (bound === 'higher' && selectedNumber > currentGuess) {
+            guessBounds.min = currentGuess;
+        } else {
+            Alert.alert('Invalid hint',
+                `The hint could not be lower or higher than your selected number (${selectedNumber})`,
+                [{text: 'Got it!', style: 'destructive'}]
+            );
+            return;
+        }
+
+        const guess = generateRandomNumber(guessBounds.min, guessBounds.max, currentGuess);
+        setCurrentGuess(guess);
+        setGuessAttempts(previousGuesses => [guess, ...previousGuesses]);
+        setTotalGuessAttempts(guessAttempts.length);
+    }
+
+    return (
+        <View style={styles.screen}>
+            <View>
+                <Title>Computer's guess</Title>
+                <View style={styles.numberContainer}>
+                    <NumberContainer>{currentGuess}</NumberContainer>
+                </View>
+
+                <Text style={styles.buttonsContainerLabel}>Is your number lower or higher?</Text>
+                <View style={styles.buttonsContainer}>
+                    <View style={styles.buttonContainer}>
+                        <PrimaryButton
+                            onPress={nextGuessHandler.bind(this, 'lower')}>Lower</PrimaryButton>
+                    </View>
+                    <View style={styles.buttonContainer}>
+                        <PrimaryButton
+                            onPress={nextGuessHandler.bind(this, 'higher')}>Higher</PrimaryButton>
+                    </View>
+                </View>
+            </View>
+            <FlatList
+                data={guessAttempts}
+                keyExtractor={(item) => item}
+                renderItem={
+                    (itemData) =>
+                        <GuessAttemptLog attemptNumber={totalGuessAttempts - itemData.index}
+                                         guess={itemData.item}/>
+                }
+            />
+        </View>
+    );
+}
+
+function generateRandomNumber(min, max, exclude) {
+    while (true) {
+        const rndNum = Math.floor(Math.random() * (max - min + 1) + min);
+        if (rndNum !== exclude) return rndNum;
+    }
+}
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        padding: '10%',
+    },
+    numberContainer: {
+        alignItems: 'center'
+    },
+    buttonsContainerLabel: {
+        fontFamily: 'open-sans',
+        fontSize: 15,
+        color: 'white',
+        textAlign: 'center',
+    },
+    buttonsContainer: {
+        flexDirection: 'row',
+    },
+    buttonContainer: {
+        flex: 1,
+    }
+})
